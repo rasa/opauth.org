80d1b3c09ff09d09bc54f26567119b2e98634691
U-Zyn Chua <chua@uzyn.com>
2012-06-13 17:54:43 +0800

    Templatize header/footer

--- callback.php
+++ callback.php
@@ -67,30 +67,8 @@ else{
 	}
 }
 ?>
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="chrome=1">
-    <title>Opauth – Multi-provider authentication framework for PHP</title>
 
-	<link rel="stylesheet" href="stylesheets/opauth.css">
-	<link rel="image_src" href="images/square.png" />
-    <script src="javascripts/scale.fix.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
-	<meta property="og:image" content="http://opauth.org/images/square.png" />
-
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="wrapper">
-      <header>
-        <h1><a href="/">Opauth</a></h1>
-        <p>Multi-provider authentication framework for PHP</p>
-      </header>
-      <section>
+<?php require('includes/head.php'); ?>
 <?php if (is_null($error)): ?>
 <?php $auth = $response['auth']; ?>
 	<h2>Authentication sucessful</h2>
@@ -173,26 +151,4 @@ else{
 	</code></pre>
 <?php endif; ?>
 
-      </section>
-    </div>
-    <footer>
-      <p class="left">Copyright © 2012 <a href="http://uzyn.com">U-Zyn Chua</a>. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-      <p class="right">Theme by <a href="https://github.com/orderedlist">orderedlist</a></p>
-	  <p class="center">Opauth is released under the MIT License.</p>
-    </footer>
-    <!--[if !IE]><script>fixScale(document);</script><!--<![endif]-->
-              <script type="text/javascript">
-            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-          </script>
-          <script type="text/javascript">
-            try {
-              var pageTracker = _gat._getTracker("UA-31914008-1");
-            pageTracker._trackPageview();
-            } catch(err) {}
-          </script>
-
-<a href="https://github.com/uzyn/opauth"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
-
-  </body>
-</html>
+<?php require('includes/foot.php'); ?>
---
+++ includes/foot.php
@@ -0,0 +1,23 @@
+      </section>
+    </div>
+    <footer>
+      <p class="left">Copyright © 2012 <a href="http://uzyn.com">U-Zyn Chua</a>. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+      <p class="right">Theme by <a href="https://github.com/orderedlist">orderedlist</a></p>
+	  <p class="center">Opauth is released under the MIT License.</p>
+    </footer>
+    <!--[if !IE]><script>fixScale(document);</script><!--<![endif]-->
+              <script type="text/javascript">
+            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+          </script>
+          <script type="text/javascript">
+            try {
+              var pageTracker = _gat._getTracker("UA-31914008-1");
+            pageTracker._trackPageview();
+            } catch(err) {}
+          </script>
+
+<a href="https://github.com/uzyn/opauth"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
+
+  </body>
+</html>
