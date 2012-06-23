a5a656c624e97c6117507be08d6f7958cb093833
U-Zyn Chua <chua@uzyn.com>
2012-06-23 21:19:08 +0800

    Download page

---
+++ download.php
@@ -0,0 +1,33 @@
+<?php require('includes/head.php'); ?>
+
+<h1>Download Opauth</h1>
+
+<div class="highlight download">
+	<p><em>Select your package:</em></p>
+	<a href="https://github.com/downloads/uzyn/opauth/Opauth-bundled-example.zip" class="button primary">Opauth bundled with examples <small>(309KB)</small></a>
+	<a href="https://github.com/downloads/uzyn/opauth/Opauth-core-latest.zip" class="button">Opauth core only <small>(24KB)</small></a>
+</div>
+
+<h2>Opauth bundled with examples</h2>
+<ul>
+	<li>Comes with the latest Opauth core.</li>
+	<li>Preinstalled with these 3 strategies: <a href="https://github.com/uzyn/opauth-facebook">Facebook</a>, <a href="https://github.com/uzyn/opauth-google">Google</a>, <a href="https://github.com/uzyn/opauth-twitter">Twitter</a></li>
+	<li>Great to get started quickly with Opauth.</li>
+	<li>Don't forget to read the <a href="https://github.com/uzyn/opauth/blob/bundled/README.md">bundled README</a></li>
+</ul>
+<p><a href="https://github.com/downloads/uzyn/opauth/Opauth-bundled-example.zip">Download ZIP</a> <small>(309KB)</small></p>
+
+<h2>Opauth core only</h2>
+<ul>
+	<li>Comes with the latest Opauth core.</li>
+	<li>Contains no bundled strategies.</li>
+</ul>
+
+<p><a href="https://github.com/downloads/uzyn/opauth/Opauth-core-latest.zip">Download ZIP</a> <small>(24KB)</small></p>
+
+
+<h2>More strategies</h2>
+<p>Extend Opauth with more strategies:</p>
+<?php require('includes/strategies.php'); ?>
+	
+<?php require('includes/foot.php'); ?>
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
 <p>Opauth as a framework provides a set of API that allows developers to <a href="https://github.com/uzyn/opauth/wiki/Strategy-Contribution-Guide">create strategies</a> that work in a predictable manner across PHP frameworks and applications.</p>
 
-<p>Opauth works well with other PHP applications & frameworks. It is currently supported on:
+<p>Opauth works well with other PHP applications &amp; frameworks. It is currently supported on:
 	<ul>
 	<li>
 	<a href="https://github.com/uzyn/opauth/tree/master/example">vanilla (plain) PHP applications</a> <small><em>(of course)</em></small>
@@ -41,85 +41,7 @@
 <p><script async class="speakerdeck-embed" data-id="4fbb28ec15a68f001f012325" data-ratio="1.4143646408839778" src="//speakerdeck.com/assets/embed.js"></script></p>
 
 <h2><a name="strategies"></a><a name="demo"></a>Available strategies</h2>
-
-<p>Provider-specific:</p>
-
-<table>
-<tr>
-<th>Strategy</th>
-    <th>Info</th>
-    <th>Download</th>
-	<th>&nbsp;</th>
-</tr>
-<tr>
-<td><img src="images/favicons/facebook.com.png"> Facebook</td>
-    <td><a href="https://github.com/uzyn/opauth-facebook">uzyn/opauth-facebook</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-facebook/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-facebook/tarball/master">TAR</a>
-    </td>
-	<td><a href="auth/facebook" class="tryme">Try me!</a></td>
-</tr>
-<tr>
-<td><img src="images/favicons/google.com.png"> Google</td>
-    <td><a href="https://github.com/uzyn/opauth-google">uzyn/opauth-google</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-google/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-google/tarball/master">TAR</a>
-    </td>
-	<td><a href="auth/google" class="tryme">Try me!</a></td>
-</tr>
-<tr>
-<td><img src="images/favicons/twitter.com.png"> Twitter</td>
-    <td><a href="https://github.com/uzyn/opauth-twitter">uzyn/opauth-twitter</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-twitter/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-twitter/tarball/master">TAR</a>
-    </td>
-	<td><a href="auth/twitter" class="tryme">Try me!</a></td>
-</tr>
-<tr>
-<td><img src="images/favicons/instagram.com.png"> Instagram</td>
-    <td><a href="https://github.com/muhdazrain/opauth-instagram">muhdazrain/opauth-instagram</a></td>
-    <td>
-        <a href="https://github.com/muhdazrain/opauth-instagram/zipball/master">ZIP</a> / 
-        <a href="https://github.com/muhdazrain/opauth-instagram/tarball/master">TAR</a>
-    </td>
-	<td><a href="auth/instagram" class="tryme">Try me!</a></td>
-</tr>
-<tr>
-<td><img src="images/favicons/linkedin.com.png"> LinkedIn</td>
-    <td><a href="https://github.com/uzyn/opauth-linkedin">uzyn/opauth-linkedin</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-linkedin/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-linkedin/tarball/master">TAR</a>
-    </td>
-	<td><a href="auth/linkedin" class="tryme">Try me!</a></td>
-</tr>
-<tr>
-<td><img src="images/favicons/openid.net.png"> OpenID</td>
-    <td><a href="https://github.com/uzyn/opauth-openid">uzyn/opauth-openid</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-openid/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-openid/tarball/master">TAR</a>
-    </td>
-	<td><a href="auth/openid" class="tryme">Try me!</a></td>
-</tr>
-<tr>
-<td><img src="images/favicons/mixi.co.jp.png"> mixi</td>
-    <td><a href="https://github.com/ritou/opauth-mixi">ritou/opauth-mixi</a></td>
-    <td>
-        <a href="https://github.com/ritou/opauth-mixi/zipball/master">ZIP</a> / 
-        <a href="https://github.com/ritou/opauth-mixi/tarball/master">TAR</a>
-    </td>
-	<td></td>
-</tr>
-
-</table>
-
-<p>Generic strategy: <a href="https://github.com/uzyn/opauth-oauth">OAuth</a></p>
-
-<p>See <a href="https://github.com/uzyn/opauth/wiki/List-of-strategies">wiki's list of strategies</a> for an updated list of Opauth strategies or to make requests.  Also, refer to <a href="https://github.com/uzyn/opauth/wiki/Strategy-contribution-guide">strategy contribution guide</a> if you would like to contribute a strategy.</p>
+<?php require('includes/strategies.php'); ?>
 
 <h2><a name="quickstart"></a>Quick start</h2>
 
