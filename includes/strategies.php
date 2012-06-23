1a3b5232dafb91a330d548cb59c4f45d2670d325
U-Zyn Chua <chua@uzyn.com>
2012-06-23 21:19:08 +0800

    Download page

---
+++ includes/strategies.php
@@ -0,0 +1,79 @@
+
+<p>Provider-specific:</p>
+
+<table>
+<tr>
+<th>Strategy</th>
+    <th>Info</th>
+    <th>Download</th>
+	<th>&nbsp;</th>
+</tr>
+<tr>
+<td><img src="images/favicons/facebook.com.png"> Facebook</td>
+    <td><a href="https://github.com/uzyn/opauth-facebook">uzyn/opauth-facebook</a></td>
+    <td>
+        <a href="https://github.com/uzyn/opauth-facebook/zipball/master">ZIP</a> / 
+        <a href="https://github.com/uzyn/opauth-facebook/tarball/master">TAR</a>
+    </td>
+	<td><a href="auth/facebook" class="tryme">Try me!</a></td>
+</tr>
+<tr>
+<td><img src="images/favicons/google.com.png"> Google</td>
+    <td><a href="https://github.com/uzyn/opauth-google">uzyn/opauth-google</a></td>
+    <td>
+        <a href="https://github.com/uzyn/opauth-google/zipball/master">ZIP</a> / 
+        <a href="https://github.com/uzyn/opauth-google/tarball/master">TAR</a>
+    </td>
+	<td><a href="auth/google" class="tryme">Try me!</a></td>
+</tr>
+<tr>
+<td><img src="images/favicons/twitter.com.png"> Twitter</td>
+    <td><a href="https://github.com/uzyn/opauth-twitter">uzyn/opauth-twitter</a></td>
+    <td>
+        <a href="https://github.com/uzyn/opauth-twitter/zipball/master">ZIP</a> / 
+        <a href="https://github.com/uzyn/opauth-twitter/tarball/master">TAR</a>
+    </td>
+	<td><a href="auth/twitter" class="tryme">Try me!</a></td>
+</tr>
+<tr>
+<td><img src="images/favicons/instagram.com.png"> Instagram</td>
+    <td><a href="https://github.com/muhdazrain/opauth-instagram">muhdazrain/opauth-instagram</a></td>
+    <td>
+        <a href="https://github.com/muhdazrain/opauth-instagram/zipball/master">ZIP</a> / 
+        <a href="https://github.com/muhdazrain/opauth-instagram/tarball/master">TAR</a>
+    </td>
+	<td><a href="auth/instagram" class="tryme">Try me!</a></td>
+</tr>
+<tr>
+<td><img src="images/favicons/linkedin.com.png"> LinkedIn</td>
+    <td><a href="https://github.com/uzyn/opauth-linkedin">uzyn/opauth-linkedin</a></td>
+    <td>
+        <a href="https://github.com/uzyn/opauth-linkedin/zipball/master">ZIP</a> / 
+        <a href="https://github.com/uzyn/opauth-linkedin/tarball/master">TAR</a>
+    </td>
+	<td><a href="auth/linkedin" class="tryme">Try me!</a></td>
+</tr>
+<tr>
+<td><img src="images/favicons/openid.net.png"> OpenID</td>
+    <td><a href="https://github.com/uzyn/opauth-openid">uzyn/opauth-openid</a></td>
+    <td>
+        <a href="https://github.com/uzyn/opauth-openid/zipball/master">ZIP</a> / 
+        <a href="https://github.com/uzyn/opauth-openid/tarball/master">TAR</a>
+    </td>
+	<td><a href="auth/openid" class="tryme">Try me!</a></td>
+</tr>
+<tr>
+<td><img src="images/favicons/mixi.co.jp.png"> mixi</td>
+    <td><a href="https://github.com/ritou/opauth-mixi">ritou/opauth-mixi</a></td>
+    <td>
+        <a href="https://github.com/ritou/opauth-mixi/zipball/master">ZIP</a> / 
+        <a href="https://github.com/ritou/opauth-mixi/tarball/master">TAR</a>
+    </td>
+	<td></td>
+</tr>
+
+</table>
+
+<p>Generic strategy: <a href="https://github.com/uzyn/opauth-oauth">OAuth</a></p>
+
+<p>See <a href="https://github.com/uzyn/opauth/wiki/List-of-strategies">wiki's list of strategies</a> for an updated list of Opauth strategies or to make requests.  Also, refer to <a href="https://github.com/uzyn/opauth/wiki/Strategy-contribution-guide">strategy contribution guide</a> if you would like to contribute a strategy.</p>
