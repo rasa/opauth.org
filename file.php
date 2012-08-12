f3769f1b639007229d6baec72f5fad21dd055e50
U-Zyn Chua <chua@uzyn.com>
2012-08-12 23:13:49 +0800

    file.php redirector for CakePHP packages

--- file.php
+++ file.php
@@ -3,6 +3,14 @@
  * Redirect to the latest Opauth downloads on GitHub
  */
 switch (strtolower(trim($_GET['name']))) {
+	case 'opauth-cakephp-sample-app.zip':
+        header('Location: https://github.com/downloads/uzyn/cakephp-opauth/Opauth-CakePHP-sample-app-0.4.1.zip');
+        break;
+
+   case 'opauth-cakephp-plugin.zip':
+        header('Location: https://github.com/downloads/uzyn/cakephp-opauth/Opauth-CakePHP-plugin-0.4.1.zip');
+        break;
+
     case 'opauth-core.zip':
     case 'core':
         header('Location: https://github.com/downloads/uzyn/opauth/Opauth-core-0.4.1.zip');
