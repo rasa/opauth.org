b1158c76fad28664f37fd5ef3f4746fa06878f2c
Ross Smith II <ross@smithii.com>
2012-11-03 00:23:36 -0700

    changes spaces back to tabs

--- includes/strategies.php
+++ includes/strategies.php
@@ -6,7 +6,7 @@
 <th>Strategy</th>
     <th>Info</th>
     <th>Download</th>
-    <th>&nbsp;</th>
+	<th>&nbsp;</th>
 </tr>
 <tr>
 <td><img src="images/favicons/facebook.com.png"> Facebook</td>
@@ -15,7 +15,7 @@
         <a href="https://github.com/uzyn/opauth-facebook/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-facebook/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/facebook" class="tryme">Try me!</a></td>
+	<td><a href="auth/facebook" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/google.com.png"> Google</td>
@@ -24,7 +24,7 @@
         <a href="https://github.com/uzyn/opauth-google/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-google/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/google" class="tryme">Try me!</a></td>
+	<td><a href="auth/google" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/twitter.com.png"> Twitter</td>
@@ -33,7 +33,7 @@
         <a href="https://github.com/uzyn/opauth-twitter/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-twitter/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/twitter" class="tryme">Try me!</a></td>
+	<td><a href="auth/twitter" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/bitbucket.com.png" width="16" height="16"> Bitbucket</td>
@@ -42,7 +42,7 @@
         <a href="https://github.com/fancyguy/opauth-bitbucket/zipball/master">ZIP</a> / 
         <a href="https://github.com/fancyguy/opauth-bitbucket/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/bitbucket" class="tryme">Try me!</a></td>
+	<td><a href="auth/bitbucket" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/disqus.com.png"> Disqus</td>
@@ -51,7 +51,7 @@
         <a href="https://github.com/rasa/opauth-disqus/zipball/master">ZIP</a> / 
         <a href="https://github.com/rasa/opauth-disqus/tarball/master">TAR</a>
     </td>
-    <td></td>
+	<td></td>
 </tr>
 <tr>
 <td><img src="images/favicons/do.com.png"> Do</td>
@@ -60,7 +60,7 @@
         <a href="https://github.com/arbales/opauth-do/zipball/master">ZIP</a> / 
         <a href="https://github.com/arbales/opauth-do/tarball/master">TAR</a>
     </td>
-    <td></td>
+	<td></td>
 </tr>
 <tr>
 <td><img src="images/favicons/flickr.com.png"> Flickr</td>
@@ -69,7 +69,7 @@
         <a href="https://github.com/pocket7878/opauth-flickr/zipball/master">ZIP</a> / 
         <a href="https://github.com/pocket7878/opauth-flickr/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/flickr" class="tryme">Try me!</a></td>
+	<td><a href="auth/flickr" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/foursquare.com.png" width="16" height="16"> Foursquare</td>
@@ -78,7 +78,7 @@
         <a href="https://github.com/pocket7878/opauth-foursquare/zipball/master">ZIP</a> / 
         <a href="https://github.com/pocket7878/opauth-foursquare/tarball/master">TAR</a>
     </td>
-    <td></td>
+	<td></td>
 </tr>
 <tr>
 <td><a name="github"></a><img src="images/favicons/github.com.png"> GitHub</td>
@@ -87,7 +87,7 @@
         <a href="https://github.com/uzyn/opauth-github/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-github/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/github" class="tryme">Try me!</a></td>
+	<td><a href="auth/github" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/instagram.com.png"> Instagram</td>
@@ -96,7 +96,7 @@
         <a href="https://github.com/muhdazrain/opauth-instagram/zipball/master">ZIP</a> / 
         <a href="https://github.com/muhdazrain/opauth-instagram/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/instagram" class="tryme">Try me!</a></td>
+	<td><a href="auth/instagram" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/linkedin.com.png"> LinkedIn</td>
@@ -105,7 +105,7 @@
         <a href="https://github.com/uzyn/opauth-linkedin/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-linkedin/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/linkedin" class="tryme">Try me!</a></td>
+	<td><a href="auth/linkedin" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/live.com.png"> (Windows) Live</td>
@@ -114,7 +114,7 @@
         <a href="https://github.com/uzyn/opauth-live/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-live/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/live" class="tryme">Try me!</a></td>
+	<td><a href="auth/live" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/mixi.co.jp.png"> mixi</td>
@@ -123,7 +123,7 @@
         <a href="https://github.com/ritou/opauth-mixi/zipball/master">ZIP</a> / 
         <a href="https://github.com/ritou/opauth-mixi/tarball/master">TAR</a>
     </td>
-    <td></td>
+	<td></td>
 </tr>
 <tr>
 <td><img src="images/favicons/paypal.com.png"> PayPal</td>
@@ -132,7 +132,7 @@
         <a href="https://github.com/24hours/opauth-paypal/zipball/master">ZIP</a> / 
         <a href="https://github.com/24hours/opauth-paypal/tarball/master">TAR</a>
     </td>
-    <td></td>
+	<td></td>
 </tr>
 <tr>
 <td><img src="images/favicons/sinaweibo.com.png"> Sina Weibo</td>
@@ -141,7 +141,7 @@
         <a href="https://github.com/dgrabla/opauth-sinaweibo/zipball/master">ZIP</a> / 
         <a href="https://github.com/dgrabla/opauth-sinaweibo/tarball/master">TAR</a>
     </td>
-    <td></td>
+	<td></td>
 </tr>
 <tr>
 <td><img src="images/favicons/openid.net.png"> OpenID</td>
@@ -150,7 +150,7 @@
         <a href="https://github.com/uzyn/opauth-openid/zipball/master">ZIP</a> / 
         <a href="https://github.com/uzyn/opauth-openid/tarball/master">TAR</a>
     </td>
-    <td><a href="auth/openid" class="tryme">Try me!</a></td>
+	<td><a href="auth/openid" class="tryme">Try me!</a></td>
 </tr>
 
 </table>
