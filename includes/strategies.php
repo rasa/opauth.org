fbd25605705987f786affb17b21568ae5ffc1a65
U-Zyn Chua <chua@uzyn.com>
2013-01-10 14:51:22 +0800

    Removed TAR download for strategies

--- includes/strategies.php
+++ includes/strategies.php
@@ -4,169 +4,151 @@
 <table>
 <tr>
 <th>Strategy</th>
-    <th>Info</th>
+    <th>Package info</th>
     <th>Download</th>
 	<th>&nbsp;</th>
 </tr>
 <tr>
 	<td><img src="images/favicons/bitbucket.com.png" width="16" height="16"> Bitbucket</td>
     <td><a href="https://github.com/fancyguy/opauth-bitbucket">fancyguy/opauth-bitbucket</a></td>
-    <td>
-        <a href="https://github.com/fancyguy/opauth-bitbucket/zipball/master">ZIP</a> / 
-        <a href="https://github.com/fancyguy/opauth-bitbucket/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/fancyguy/opauth-bitbucket/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/bitbucket" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/disqus.com.png"> Disqus</td>
     <td><a href="https://github.com/rasa/opauth-disqus">rasa/opauth-disqus</a></td>
-    <td>
-        <a href="https://github.com/rasa/opauth-disqus/zipball/master">ZIP</a> / 
-        <a href="https://github.com/rasa/opauth-disqus/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/rasa/opauth-disqus/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 <td><img src="images/favicons/do.com.png"> Do</td>
     <td><a href="https://github.com/arbales/opauth-do">pocket7878/opauth-do</a></td>
-    <td>
-        <a href="https://github.com/arbales/opauth-do/zipball/master">ZIP</a> / 
-        <a href="https://github.com/arbales/opauth-do/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/arbales/opauth-do/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/facebook.com.png"> <strong>Facebook</strong></td>
     <td><a href="https://github.com/uzyn/opauth-facebook">uzyn/opauth-facebook</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-facebook/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-facebook/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-facebook/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/facebook" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/flickr.com.png"> Flickr</td>
     <td><a href="https://github.com/pocket7878/opauth-flickr">pocket7878/opauth-flickr</a></td>
-    <td>
-        <a href="https://github.com/pocket7878/opauth-flickr/zipball/master">ZIP</a> / 
-        <a href="https://github.com/pocket7878/opauth-flickr/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/pocket7878/opauth-flickr/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/flickr" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/foursquare.com.png" width="16" height="16"> Foursquare</td>
     <td><a href="https://github.com/pocket7878/opauth-foursquare">pocket7878/opauth-foursquare</a></td>
-    <td>
-        <a href="https://github.com/pocket7878/opauth-foursquare/zipball/master">ZIP</a> / 
-        <a href="https://github.com/pocket7878/opauth-foursquare/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/pocket7878/opauth-foursquare/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 <td><a name="github"></a><img src="images/favicons/github.com.png"> GitHub</td>
     <td><a href="https://github.com/uzyn/opauth-github">uzyn/opauth-github</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-github/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-github/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-github/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/github" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/google.com.png"> <strong>Google</strong></td>
     <td><a href="https://github.com/uzyn/opauth-google">uzyn/opauth-google</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-google/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-google/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-google/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/google" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 <td><img src="images/favicons/instagram.com.png"> Instagram</td>
     <td><a href="https://github.com/muhdazrain/opauth-instagram">muhdazrain/opauth-instagram</a></td>
-    <td>
-        <a href="https://github.com/muhdazrain/opauth-instagram/zipball/master">ZIP</a> / 
-        <a href="https://github.com/muhdazrain/opauth-instagram/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/muhdazrain/opauth-instagram/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/instagram" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/linkedin.com.png"> LinkedIn</td>
     <td><a href="https://github.com/uzyn/opauth-linkedin">uzyn/opauth-linkedin</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-linkedin/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-linkedin/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-linkedin/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/linkedin" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/live.com.png"> (Windows) Live</td>
     <td><a href="https://github.com/uzyn/opauth-live">uzyn/opauth-live</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-live/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-live/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-live/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/live" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/mixi.co.jp.png"> mixi</td>
     <td><a href="https://github.com/ritou/opauth-mixi">ritou/opauth-mixi</a></td>
-    <td>
-        <a href="https://github.com/ritou/opauth-mixi/zipball/master">ZIP</a> / 
-        <a href="https://github.com/ritou/opauth-mixi/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/ritou/opauth-mixi/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/openid.net.png"> OpenID</td>
     <td><a href="https://github.com/uzyn/opauth-openid">uzyn/opauth-openid</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-openid/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-openid/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-openid/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/openid" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/paypal.com.png"> PayPal</td>
     <td><a href="https://github.com/24hours/opauth-paypal">24hours/opauth-paypal</a></td>
-    <td>
-        <a href="https://github.com/24hours/opauth-paypal/zipball/master">ZIP</a> / 
-        <a href="https://github.com/24hours/opauth-paypal/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/24hours/opauth-paypal/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/sinaweibo.com.png"> Sina Weibo (新浪微博)</td>
     <td><a href="https://github.com/dgrabla/opauth-sinaweibo">dgrabla/opauth-sinaweibo</a></td>
-    <td>
-        <a href="https://github.com/dgrabla/opauth-sinaweibo/zipball/master">ZIP</a> / 
-        <a href="https://github.com/dgrabla/opauth-sinaweibo/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/dgrabla/opauth-sinaweibo/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/twitter.com.png"> <strong>Twitter</strong></td>
     <td><a href="https://github.com/uzyn/opauth-twitter">uzyn/opauth-twitter</a></td>
-    <td>
-        <a href="https://github.com/uzyn/opauth-twitter/zipball/master">ZIP</a> / 
-        <a href="https://github.com/uzyn/opauth-twitter/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/uzyn/opauth-twitter/zipball/master">ZIP</a>
     </td>
 	<td><a href="auth/twitter" class="tryme">Try me!</a></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/vk.com.png"> VKontakte</td>
     <td><a href="https://github.com/dgrabla/opauth-vkontakte">dgrabla/opauth-vkontakte</a></td>
-    <td>
-        <a href="https://github.com/dgrabla/opauth-vkontakte/zipball/master">ZIP</a> / 
-        <a href="https://github.com/dgrabla/opauth-vkontakte/tarball/master">TAR</a>
+    <td class="center">
+        <a href="https://github.com/dgrabla/opauth-vkontakte/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
 <tr>
 	<td><img src="images/favicons/developer.yahoo.co.jp.png" width="16" height="16"> Yahoo! Japan</td>
     <td><a href="https://github.com/ritou/opauth-yahoojp">ritou/opauth-yahoojp</td>
-    <td>
-        <a href="https://github.com/ritou/opauth-yahoojp/zipball/master">ZIP</a> / 
-        <a href="https://github.com/ritou/opauth-yahoojp/tarball/master">TAR</a>
+   	<td class="center">
+        <a href="https://github.com/ritou/opauth-yahoojp/zipball/master">ZIP</a>
     </td>
 	<td></td>
 </tr>
